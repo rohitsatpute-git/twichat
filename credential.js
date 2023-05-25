08f245ec5f851f0673b890a0df9ee6ebd5ff9671
ROHIT SATPUTE <rohitsatpute480@gmail.com>
2023-05-25 12:20:42 +0530

    changing to main branch

---
+++ credential.js
@@ -0,0 +1,7 @@
+
+module.exports={
+    consumer_key:'NsBreycH0YrlPTYTjgcBAs2cH',
+    consumer_secret:'UoArYi3Iv1SSw2oeANf1mzJmAoLJDhZwvuE498I4PsgXbOQ5d4',
+    access_token:'1435189114140119041-0zlbbEleokNRhDVaPJAIDUBVdklW0C',
+    access_token_secret:'UrwAnP9hg27XA5yX17Z3O3pDeFuAn8CRZmInAaJEBClVe'
+}
