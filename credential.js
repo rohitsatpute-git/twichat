49ff066e83198e700fce43cb20f7ff710c98337e
ROHIT SATPUTE <rohitsatpute480@gmail.com>
2023-05-25 13:36:59 +0530

    added crederntial

---
+++ credential.js
@@ -0,0 +1,7 @@
+module.exports={
+    consumer_key:'HBlQQx0LAVJD5SuHgEhqtOxkG',
+    consumer_secret:'ohUVjhpO1a0WSGACPlx5xtGM2YwUhEbSEoRUi3im8cT0ak0zgb',
+    access_token:'1435189114140119041-G3NLOHVtXbGwq5asb6HRU7i03IIbQm',
+    access_token_secret:'yP6S7DtgZ4AT12gYZkcY0yDlTzfjNjv4prtm3aq7uHoFy'
+
+};
